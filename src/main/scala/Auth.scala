886902672a0c6de180b7d8882e59cafbe4a05daa
Andre Azevedo <andre.azevedo@gmail.com>
2012-03-19 23:51:42 -0300

    Completing oauth authentication by adding the access_token() method.

--- src/main/scala/Auth.scala
+++ src/main/scala/Auth.scala
@@ -9,4 +9,14 @@ object Auth {
 	
 	def authorize_uri(client_id: String, redirect_uri: String) = 
 		svc.secure / "authorize" <<? Map("client_id" -> client_id, "redirect_uri" -> redirect_uri)
+		
+	def access_token(client_id: String, redirect_uri: String, client_secret: String, code: String) =
+		svc.secure.POST / "access_token" <:< 
+			Map("client_id" -> client_id, 
+				"redirect_uri" -> redirect_uri,
+				"client_secret" -> client_secret,
+				"code" -> code
+			) >% {
+				m => m("access_token")
+			}
 }
