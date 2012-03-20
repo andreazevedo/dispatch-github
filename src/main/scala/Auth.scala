558a76368081e1d9dfe698f761ad1ff3e916e066
Andre Azevedo <andre.azevedo@gmail.com>
2012-03-19 23:23:38 -0300

    Improving authorize_uri()

--- src/main/scala/Auth.scala
+++ src/main/scala/Auth.scala
@@ -8,5 +8,5 @@ object Auth {
 	val svc = :/("github.com") / "login" / "oauth"
 	
 	def authorize_uri(client_id: String, redirect_uri: String) = 
-		(svc / "authorize").secure <<? Map("client_id" -> client_id, "redirect_uri" -> redirect_uri)
+		svc.secure / "authorize" <<? Map("client_id" -> client_id, "redirect_uri" -> redirect_uri)
 }
