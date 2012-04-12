9d4bf012c454d18531e08d744b5cd759bcec3f3a
Andre Azevedo <andre.azevedo@gmail.com>
2012-04-12 00:15:59 -0300

    Correct repository parsing

--- src/main/scala/GhRepository.scala
+++ src/main/scala/GhRepository.scala
@@ -8,7 +8,7 @@ import JsHttp._
 import java.util.Date
 import java.text.SimpleDateFormat
 
-case class GhRepository(id:Int, owner_login:String, name:String, updatedAt:Date, language:String,
+case class GhRepository(id:Int, owner_login:String, name:String, updated_at:Date, language:String,
 	html_url:String, clone_url:String, description:String, open_issues:Int)
 
 object GhRepository {
@@ -25,14 +25,14 @@ object GhRepository {
 				val id:Int = num(jsonObj.self(JsString("id"))).intValue
 				val owner_login = jsonOwnerObj.self(JsString("login")).self.toString
 				val name = jsonObj.self(JsString("name")).self.toString
-				val updatedAt:Date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(jsonObj.self(JsString("updatedAt")).self.toString)
-				val language = jsonObj.self(JsString("language")).self.toString
+				val updated_at:Date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(jsonObj.self(JsString("updated_at")).self.toString.replace("T", " ").replace("Z", ""))
+				val language = jsonObj.self(JsString("language")).self.asInstanceOf[String]
 				val html_url = jsonObj.self(JsString("html_url")).self.toString
 				val clone_url = jsonObj.self(JsString("clone_url")).self.toString
 				val description = jsonObj.self(JsString("description")).self.toString
 				val open_issues:Int = num(jsonObj.self(JsString("open_issues"))).intValue
 
-				GhRepository(id, owner_login, name, updatedAt, language, html_url, clone_url, description, open_issues)
+				GhRepository(id, owner_login, name, updated_at, language, html_url, clone_url, description, open_issues)
 			}
 		}
 	}
