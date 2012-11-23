da08c26aead2613275238158cbd3553b567ae8fd
andreazevedo <andre.azevedo@gmail.com>
2012-11-23 16:02:10 -0200

    Added method to get a single organization

--- src/main/scala/dispatch/github/GhOrganization.scala
+++ src/main/scala/dispatch/github/GhOrganization.scala
@@ -1,23 +1,18 @@
 package dispatch.github
 
 import dispatch._
-import json._
-import JsHttp._
+import json.JsHttp._
+import net.liftweb.json._
+import net.liftweb.json.Serialization.write
 
 case class GhOrganization(id: Int, login: String, url: String, avatar_url: String) {
 
 	def members(access_token:String) = {
 		val svc = GitHub.api_host / "orgs" / login / "members"
 		svc.secure <<? Map("access_token" -> access_token) ># { json =>
-			val jsonList = parse.jsonList(json)
-
-			jsonList.map { jsonObj =>
-				val id: Int = jsonObj("id").asInt
-				val login: String = jsonObj("login").asString
-				val avatar_url = jsonObj("avatar_url").asString
-
-				GhContributor(id, login, avatar_url)
-			}
+			implicit val formats = DefaultFormats
+			val jsonObj = JsonParser.parse(json.toString)
+			jsonObj.extract[List[GhContributor]]
 		}
 	}
 
@@ -30,18 +25,20 @@ object GhOrganization {
 	def get_organizations(access_token: String) = {
 		val svc = GitHub.api_host / "user" / "orgs"
 		svc.secure <<? Map("access_token" -> access_token) ># { json =>
-			val jsonList = parse.jsonList(json)
-
-			jsonList.map { jsonObj =>
-				
-				val id: Int = jsonObj("id").asInt
-				val login = jsonObj("login").asString
-				val url = jsonObj("url").asString
-				val avatar_url = jsonObj("avatar_url").asString
-
-				GhOrganization(id, login, url, avatar_url)
-			}
+			implicit val formats = DefaultFormats
+			val jsonObj = JsonParser.parse(json.toString)
+			jsonObj.extract[List[GhOrganization]]
 		}
 	}
+
+	def get_organization(org: String, access_token: String) = {
+		val svc = GitHub.api_host / "orgs" / org
+		svc.secure <<? Map("access_token" -> access_token) ># { json =>
+			implicit val formats = DefaultFormats
+			val jsonObj = JsonParser.parse(json.toString)
+			jsonObj.extract[GhOrganization]
+		}
+	}
+
 		
 }
