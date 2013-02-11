48fafcc4c57f8d0558114a90da6b5ae5012f6e34
Eran Meir <eranmeir@gmail.com>
2013-02-11 03:47:22 +0200

    porting OAuth support

--- src/test/scala/AuthSpec.scala
+++ src/test/scala/AuthSpec.scala
@@ -7,17 +7,15 @@ import dispatch.github._
 class AuthSpec extends Specification {
 	val clientId:String = "ad42e1f93976bcaa61f2"
 	val clientSecret:String = "4d7eb4d41efbcb9c25e3884526991d2bed397591"
-	val	redirectUri:String = "dispatchgithubtests.com"
+	val redirectUri:String = "dispatchgithubtests.com"
 
 	"When getting the github authorization url" should {
 
-		"return an object of the dispatch.Request when calling Auth.authorize_uri" in {
-			Auth.authorize_uri(clientId, redirectUri) must haveClass[com.ning.http.client.RequestBuilder]
-		}
-
-		"return a request that points to the correct url when calling Auth.authorize_uri" in {
-			//Auth.authorize_uri(clientId, redirectUri).build().getUrl() must equalTo("https://github.com/login/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUri)
-         Pending("Pending - not supporting OAuth just yet")
+		"return a Promise that contains to the correct url when calling Auth.webapp_authorize_uri" in {
+			val authRes = Auth.webapp_authorize_uri(clientId, redirectUri)
+         val redirectStr = authRes()
+         
+         redirectStr must equalTo("https://github.com/login?return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D" + clientId + "%26redirect_uri%3D" + redirectUri)
 		}
 	}
 }
