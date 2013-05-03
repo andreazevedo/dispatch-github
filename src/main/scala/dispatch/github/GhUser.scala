79476e65688022d5d44e52ebfa238cf456ec4196
juandebravo <juandebravo@gmail.com>
2013-05-03 23:34:18 +0200

    Use **type** instead of **account_type**
    GhUser is defining account_type as attribute, but
    account_type cannot be unserialized directly
    as the JSON field retrieved from github is type.
    
    We could either define a post processor or use
    back ticks to use type as valid attribute (type
    is a reserved word)
    
    Also, a method to retrieve an user profile without
    access token has been added

--- src/main/scala/dispatch/github/GhUser.scala
+++ src/main/scala/dispatch/github/GhUser.scala
@@ -11,17 +11,23 @@ case class GhAuthorSummary(name:String, date:Date, email:String)
 
 case class GhAuthor(avatar_url: String, url: String, login: String, gravatar_id: String, id: Int)
 
-case class GhUser(id: Int, login: String, name: String, email: String, avatar_url: String, account_type: String)
+case class GhUser(id: Int, login: String, name: String, email: String, avatar_url: String, `type`: String)
 
 case class GhOwner(id:Int, login: String)
 
 
 object GhUser {
 	implicit val formats = DefaultFormats
-   
+
 	def get_authenticated_user(access_token: String) = {
 		val svc = GitHub.api_host / "user"
 		val userJson = Http(svc.secure <<? Map("access_token" -> access_token) OK as.lift.Json)
       for (js <- userJson) yield js.extract[GhUser]
 	}
+
+    def get_user(username: String) = {
+        val svc = GitHub.api_host / "users" / username
+        val userJson = Http(svc.secure OK as.lift.Json)
+      for (js <- userJson) yield js.extract[GhUser]
+    }
 }
